2eb6f32c4d04e670c8523239246d1277a85fa1b8
mamaz <hisma.sudradjat@btpn.com>
2017-06-17 15:17:06 +0700

    Updates link

--- src/Services/GasolineServices.js
+++ src/Services/GasolineServices.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // const GASOLINE_URL = 'https://api.myjson.com/bins/hwi9z';
 // const GASOLINE_URL = 'https://api.myjson.com/bins/ovphj';
-const GASOLINE_URL = 'https://api.myjson.com/bins/izilj';
+// const GASOLINE_URL = 'https://api.myjson.com/bins/izilj';
+const GASOLINE_URL = 'https://api.myjson.com/bins/1gbsh3';
 
 // will return promise
 const getGasolineData = () => {
